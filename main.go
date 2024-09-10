9c108fed28c5d7c5cbb7bca245ed774108729d95
George KND <jordgo1095@gmail.com>
2024-09-10 13:33:15 +0300

    last commit

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package openvpn
+package main
 
 import (
 	"bytes"
@@ -11,16 +11,25 @@ import (
 	"time"
 )
 
+//go build -o conn_openvpn
+//sudo ./conn_openvpn --conffile=/home/george/mabat/mabat_docs/vpn/sslvpn-gh106-client-config.ovpn --authfile='/home/george/mabat/mabat_docs/vpn/cred.txt'
 
 var (
 	conffile = flag.String("conffile", "", "openVPN client conf file")
 	authfile = flag.String("authfile", "", "openVPN client credentials file")
+	ipsStr = flag.String("ips", "182.48.202.50,10.255.0.1", "IPs to conection, listed separated by commas, default is <182.48.202.50,10.255.0.1>")
 )
 
 func main() {
-	closeCh := make(chan int, 2)
+	flag.Parse()
+	slog.Info("Strted with:")
+	slog.Info("Configuration", "file", conffile)
+	slog.Info("Auth:", "file", authfile)
 
-	go checkConnection(closeCh)
+	closeCh := make(chan int, 2)
+	ips := strings.Split(strings.ReplaceAll(*ipsStr, " ", ""), ",")
+
+	go checkConnection(closeCh, ips)
 
 	for {
 		ctx, cancel := context.WithCancel(context.Background())
@@ -32,31 +41,53 @@ func main() {
 }
 
 // checking connection using curl request
-func checkConnection(closeCh chan int) string {
+func checkConnection(closeCh chan int, ips []string) string {
 	for {
-		time.Sleep(15*time.Second)
+		time.Sleep(20*time.Second)
 		slog.Info("Checking Connection...")
 
-		cmd := exec.Command("curl", "ipinfo.io")
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+		cmd := exec.CommandContext(ctx, "curl", "ipinfo.io")
 
 		var out bytes.Buffer
 		cmd.Stdout = &out
+		cmd.Stderr = os.Stdout
 	
 		err := cmd.Run()
 		if err != nil {
 			slog.Error("CMD Error of checkConnection", "error", err)
+			closeCh <- 1
+			continue
 		}
 
 		res := out.String()
 
 		println(res)
 
-		if strings.Contains(res, "hostname") {
-			slog.Info("Connected!")
-			return "ok"
+		if !strings.Contains(res, "hostname") {
+			slog.Info("Reconnection...", "reason", "hostname not found")
+			closeCh <- 1
+			continue
 		}
-		
-		closeCh <- 1
+
+		isContainsAllIps := true
+		// checkiscontains:
+		for _, ip := range ips {
+			if !strings.Contains(res, ip) {
+				isContainsAllIps = false
+				break
+			}
+		}
+
+		if len(ips) > 0 && !isContainsAllIps {
+			slog.Info("Reconnection...", "reason", "ip not found", "ips", ips)
+			closeCh <- 1
+			continue
+		}
+
+		slog.Info("Connected!")
+		return "ok"
 	}
 
 
--- main_test.go
+++ main_test.go
@@ -1,20 +1,38 @@
-package openvpn
+package main
 
 import (
-	"log/slog"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCheckConnection(t *testing.T) {
-	closeCh := make(chan int, 2)
+func TestLongCheckConnection(t *testing.T) {
+	t.Parallel()
 
-	res := checkConnection(closeCh)
-	time.Sleep(16*time.Second)
-	
-	if res != "ok" {
-		panic("Alarm!!!")
+	maxDuration := 27*time.Second
+	done := make(chan string)
+
+	go func() {
+		closeCh := make(chan int, 2)
+		ips := []string{"85.174.51.235"}
+		// emptyIps := []string{}
+		res := checkConnection(closeCh, ips)
+		done <- res
+
+	}()
+
+	select {
+	case res := <- done:
+		exp := "ok"
+		assert.EqualValues(t, exp, res, "Success")
+
+	case <- time.After(maxDuration):
+		t.Errorf("Test took longer than %v", maxDuration)
 	}
 
-	slog.Info("OK!")
+}
+
+func TestSome(t *testing.T) {
+	assert.True(t, true, "Stub")
 }
